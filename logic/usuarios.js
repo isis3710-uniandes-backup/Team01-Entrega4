5cde5cd1a38ed82c2bfff7c2990f91b87f34a53e
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-10-24 08:47:48 -0500

    login and register

--- logic/usuarios.js
+++ logic/usuarios.js
@@ -1,5 +1,6 @@
+let jwt = require('jsonwebtoken');
 const mongoClient = require("mongodb").MongoClient;
-const { databaseUser, databasePassword, databaseName } = require('../config');
+const { databaseUser, databasePassword, databaseName, secretKey } = require('../config');
 const uri = "mongodb+srv://"+databaseUser+":"+databasePassword+"@educapp-viylh.gcp.mongodb.net/test?retryWrites=true&w=majority";
 let conn = mongoClient.connect(uri, {
     useNewUrlParser : true,
@@ -36,6 +37,53 @@ class Usuarios{
         conn.then(client => {
             let user = client.db(databaseName).collection("usuarios").findOne({_id : username});
             res.send(user);
+        })
+    }
+    /**
+     * Sign up of an user
+     * @param {*} req 
+     * @param {*} res 
+     */
+    register(req,res)
+    {
+        let newUser = req.body;
+        conn.then(client => {
+            client.db(databaseName).collection("usuarios").insertOne(newUser, (err,data) => {
+                if(err) 
+                {
+                    res.status(400).send('Inserción invalida. Revise los datos suministrados');
+                    throw err;
+                }
+                res.send(data);
+            })
+        })
+    }
+    /**
+     * Log in of an user
+     * @param {*} req 
+     * @param {*} res 
+     */
+    login(req, res)
+    {
+        let username = req.body.username;
+        let password = req.body.password;
+
+        conn.then(client => {
+           let user = client.db(databaseName).collection("usuarios").findOne({_id : username});
+           if(user)
+           {
+               if(user.password ===  password)
+               {
+                  let newToken = jwt.sign({username : username },secretKey, {expiresIn : '3h'} );
+                  //necesito investigar si desde aqui la cookie queda para el navegador del cliente
+                  res.cookie('SESSIONID', newToken, {httpOnly : true, secure : true});
+                  res.send('Login existoso');
+               }
+               else
+               {
+                   res.status(400).send('Contraseña incorrecta');
+               }
+           }
         })
     }
 }
