7270e485188e56e541583292aa7c1088e5c3089a
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-10-24 08:03:44 -0500

    Added all the get routes

--- logic/programs.js
+++ logic/programs.js
@@ -1,3 +1,89 @@
-class Programs{
-    
+const mongoClient = require("mongodb").MongoClient;
+const { databaseUser, databasePassword, databaseName } = require('../config');
+const uri = "mongodb+srv://"+databaseUser+":"+databasePassword+"@educapp-viylh.gcp.mongodb.net/test?retryWrites=true&w=majority";
+let conn = mongoClient.connect(uri, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
+.catch(error => {
+    console.log(error);
+    //defined a response with an error to be shown 
+});
+class Programs
+{
+    /**
+     * Get all the programs.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getPrograms(req,res)
+    {
+        conn.then(client => {
+            client.db(databaseName).collection("programas").find({})
+            .toArray((err,data) => {
+                if(err) {
+                    res.status(500).send('El servidor está caído, intente más tarde.');
+                    throw err;
+                }
+                res.send(data);
+            })
+        })
+    }
+    /**
+     * Get all the programs of an specific area
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getProgramsOfAarea(req,res)
+    {
+        let area = req.params.area;
+        conn.then(client => {
+            client.db(databaseName).collection("programas").find({area : area})
+            .toArray((err,data) => {
+                if(err) {
+                    res.status(500).send('El servidor está caído, intente más tarde.');
+                    throw err;
+                }
+                res.send(data);
+            })
+        })
+    }
+    /**
+     * Get the universities of an specific program
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getUniversitiesOfProgram(req,res)
+    {
+        let nombrePrograma = req.params.nombre;
+        conn.then(client => {
+          let program =  client.db(databaseName).collection("programas").findOne({nombre : nombrePrograma});
+          if(program.universidades.length > 0)
+          {
+              conn.then(client => {
+                  client.db(databaseName).collection("universidades").find({_id : {$in : program.universidades}})
+                  .toArray((err,data) => {
+                    if(err) {
+                        res.status(500).send('El servidor está caído, intente más tarde.');
+                        throw err;
+                    }
+                    res.send(data);
+                  })
+              })
+          }
+          else{
+              res.send([]);
+          }
+        })
+    }
+    getProgram(req,res)
+    {
+        let nombrePrograma = req.params.nombre;
+        conn.then(client => {
+           let theProgram = client.db(databaseName).collection("programas").findOne({nombre : nombrePrograma});
+           res.send(theProgram);
+        })
+    }
+
 }
+module.exports = Programs;
--- logic/universities.js
+++ logic/universities.js
@@ -1,5 +1,3 @@
-var express = require('express');
-var router = express.Router();
 const mongoClient = require("mongodb").MongoClient;
 const { databaseUser, databasePassword, databaseName } = require('../config');
 const uri = "mongodb+srv://"+databaseUser+":"+databasePassword+"@educapp-viylh.gcp.mongodb.net/test?retryWrites=true&w=majority";
@@ -12,5 +10,78 @@ let conn = mongoClient.connect(uri, {
 });
 class Universities
 {
+    /**
+     * Get all the universities
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getListUniversities(req, res)
+    {
+        conn.then(client => {
+            client.db(databaseName).collection("universidades").find({})
+            .toArray((err,data) => {
+                if(err) {
+                    res.status(500).send('El servidor está caído, intente más tarde.');
+                    throw err;
+                }
+                res.send(data);
+            })
+        })
+    }
+    /**
+     * Get an university by its name or nickname
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getUniversity(req,res)
+    {
+        let n = req.params.username;
+        let nUppercased = n.toUpperCase();
+        conn.then(client => {
+            client.db(databaseName).collection("universidades").find({$or : [{nombre : nUppercased}, {nickname : nUppercased}]})
+            .toArray((err,data)=> {
+                if(err) {
+                    res.status(500).send('El servidor está caído, intente más tarde.');
+                    throw err;
+                }
+                res.send(data);
+            })
+        });
+    }
+    /**
+     * Get all the programs of an university
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getProgramsOfUniversity(req,res)
+    {
+        let n = req.params.username;
+        let nUppercased = n.toUpperCase();
+        conn.then(client => {
+            client.db(databaseName).collection("universidades").find({$or : [{nombre : nUppercased}, {nickname : nUppercased}]})
+            .toArray((err,data)=> {
+                if(err) {
+                    res.status(500).send('El servidor está caído, intente más tarde.');
+                    throw err;
+                }
+                else if(data.lenght > 0)
+                {
+                    client.db(databaseName).collection("programas").find({_id : {$in : data[0].programas }})
+                    .toArray((err,data) => {
+                        if(err) {
+                            res.status(500).send('El servidor está caído, intente más tarde.');
+                            throw err;
+                        }
+                        res.send(data);
+                    })
+                }
+                else
+                {
+                    res.send([]);
+                }
+            })
+        });
 
+    }
 }
+module.exports = Universities;
