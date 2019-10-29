a6de347eb8aed50197ced8cd2cb093a6ed8d569e
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2019-10-28 22:41:09 -0500

    navbar moviles

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -6,7 +6,7 @@ export default class navbar extends Component{
     render() {
         return(
             <div className="container">
-                <nav id="menu" className="menu">
+                <nav id="menu" className="menu d-none d-md-block">
                     <div className="logo"><i className="fas fa-home"></i></div>
                     <div className="menu_section section_1">Section 1</div>
                     <div className="menu__wrap_1">
@@ -29,6 +29,33 @@ export default class navbar extends Component{
                     </div>
                 </nav>
 
+                <nav id="menu_movil" className="menu_movil d-block d-sm-block d-md-none">
+                    <div className="row">
+                        <div className="col-5">
+                            <div className="row">
+                                <div className="col-6">
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                </div>
+                                <div className="col-6">
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="logo_movil"><a href="#"><i className="fas fa-home"></i></a></div>
+                        </div>
+                        <div className="col-5">
+                            <div className="row">
+                                <div className="col-6">
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                </div>
+                                <div className="col-6">
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </div>
         )
     }
