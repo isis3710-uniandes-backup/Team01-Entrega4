7c2dc40ddc0a761ac0aeb9327a85de09ec827188
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2019-10-30 14:59:06 -0500

    Avance

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import '../styles/navbar.css';
+const Swal = require('sweetalert2')
 
 export default class navbar extends Component{
 
+    mostrar(){
+        Swal.fire({
+            title: 'Comentario',
+            input: 'textarea',
+            inputPlaceholder: 'Type your message here...',
+            inputAttributes: {
+                'aria-label': 'Type your message here'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Comentar',
+            cancelButtonText: 'Cancelar'
+            })
+            .then((value)=>{
+                console.log(value['value'])
+            })
+    }
     render() {
         return(
             <div className="container">
@@ -11,20 +28,18 @@ export default class navbar extends Component{
                     <div className="menu_section section_1">Section 1</div>
                     <div className="menu__wrap_1">
                         <ul data-menu="main" className="menu__level">
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 2</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 3</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 4</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Home</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Universidades</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Carreras</a></li>
                         </ul>
                     </div>
 
                     <div className="menu_section section_2">Section 2</div>
                     <div className="menu__wrap_2">
                         <ul data-menu="main" className="menu__level">
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 2</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 3</a></li>
-                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Item 4</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Perfil</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Ingresar</a></li>
+                            <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Registrarse</a></li>
                         </ul>
                     </div>
                 </nav>
