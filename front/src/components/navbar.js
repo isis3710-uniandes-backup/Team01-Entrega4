378cd327de584b609079893f13449b9eb888cdac
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2019-11-02 12:27:43 -0500

    Avanzando

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -25,7 +25,7 @@ export default class navbar extends Component{
             <div className="container">
                 <nav id="menu" className="menu d-none d-md-block">
                     <div className="logo"><i className="fas fa-home"></i></div>
-                    <div className="menu_section section_1">Section 1</div>
+                    <div className="menu_section section_1">Navegación</div>
                     <div className="menu__wrap_1">
                         <ul data-menu="main" className="menu__level">
                             <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Home</a></li>
@@ -34,7 +34,7 @@ export default class navbar extends Component{
                         </ul>
                     </div>
 
-                    <div className="menu_section section_2">Section 2</div>
+                    <div className="menu_section section_2">Configuración</div>
                     <div className="menu__wrap_2">
                         <ul data-menu="main" className="menu__level">
                             <li className="menu__item"><a className="menu__link" href="#"><i className="fas fa-igloo"></i>Perfil</a></li>
@@ -49,10 +49,10 @@ export default class navbar extends Component{
                         <div className="col-5">
                             <div className="row">
                                 <div className="col-6">
-                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Universidades</a></li>
                                 </div>
                                 <div className="col-6">
-                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Carreras</a></li>
                                 </div>
                             </div>
                         </div>
@@ -65,7 +65,7 @@ export default class navbar extends Component{
                                     <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
                                 </div>
                                 <div className="col-6">
-                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Item 1</a></li>
+                                    <li className="menu_movil__item"><a className="menu_movil__link" href="#"><i className="fas fa-igloo"></i>Ingresar</a></li>
                                 </div>
                             </div>
                         </div>
