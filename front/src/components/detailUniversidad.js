95af947c35b5b3790f5188e0be0c20863b7e39d5
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-11-05 09:57:41 -0500

    Uppercasing params

--- front/src/components/detailUniversidad.js
+++ front/src/components/detailUniversidad.js
@@ -21,7 +21,7 @@ export default class universidad extends Component {
         return (
             <div id="card" className="col-12 marginBottom">
                  <Link id="link" to ={{
-                    pathname: `/universidad/+${this.state.nombre}+/programa/${this.state.programa}`}} >
+                    pathname: `/universidad/+${this.state.nombre.toUpperCase()}+/programa/${this.state.programa.toUpperCase()}`}} >
                     <Card id="Universidad" >
                             <Card.Header>
                              <h5 id="nombreuniversidad" ><strong>{this.state.nombre}</strong></h5>  
